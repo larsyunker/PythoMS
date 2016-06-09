866071f6978e5dd96a4d1295830f512b7626970a
larsyunker <lars.yunker@gmail.com>
2016-06-09 16:50:55 -0700

    ED-ESI upload
    beta version for incorporation into Darien's script

---
+++ ED-ESI plot.py
@@ -0,0 +1,58 @@
+"""
+for use with EDESI pulling
+
+new:
+    I hope this works for Darien
+    ---0.1 beta---
+
+"""
+import sys
+from _mzML import mzML
+from tome_v02 import binnspectra,bincidspectra
+
+filename = 'HZ-140516_HOTKEYMSMS 1376 II.raw' # raw or mzml file name
+fillzeros = True # fills spectrum with zeros
+decpl = 1 # number of decimal places to track
+threshold = 20 # threshold intensity value
+mzrange=None # mzrange to track
+sr = 'all' # scan range to track
+
+mzml = mzML(filename,verbose=True)
+
+# pull time list, each spectrum at those time points, the scan range, and the mz range
+#speclist,sr,mzrange = mzml.pullspectra(mzrange=mzrange,sr=sr)
+msms,limits = mzml.pullmsmsspectra()
+binned = {}
+grouped = {}
+voltage = {}
+zvals = {} # intensity matrix
+
+for ion in msms:
+    binned[ion] = binnspectra(msms[ion], len(msms[ion]), dec=decpl, startmz=limits[ion][0], endmz=limits[ion][1])[0] # bins all spectra together
+    grouped[ion],voltage[ion] = bincidspectra(msms[ion], dec=decpl, startmz=limits[ion][0], endmz=limits[ion][1], threshold=threshold, fillzeros=fillzeros)
+    for ind,scan in enumerate(grouped[ion]):
+        sys.stdout.write('\rExtracting z values #%d/%d (%.1f%%) ' %(ind+1,len(grouped),float(ind+1)/float(len(grouped))*100.))
+        if zvals.has_key(ion) is False:
+            zvals[ion] = []
+        zvals[ion].append(scan[1]) # append only the intensities to list
+    sys.stdout.write(' DONE\n')
+
+"""
+each key in msms, binned, grouped, and voltage is a single ion that is being tracked
+subkeys in msms are timepoints, containing subkeys for collision energy, x, and y
+subkeys in binned is a [[m/z],[intensities]] structure which is all spectra binned
+subkeys in grouped are voltages, which each have a [[m/z],[intensities]] spectrum
+voltage[ion] is a list of voltages
+
+axes for ED ESI plots
+for each ion:
+    xvalues = grouped[ion][0][0]
+    yvalues = voltage[ion]
+    z values = zvals[ion]
+"""
+
+
+#for ind,scan in enumerate(grouped):
+#    sys.stdout.write('\rExtracting z values #%d/%d (%.1f%%) ' %(ind+1,len(grouped),float(ind+1)/float(len(grouped))*100.))
+#    zvals.append(scan[1]) # append only the intensities to list
+#sys.stdout.write(' DONE\n')
