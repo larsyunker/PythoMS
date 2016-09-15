6ec7fcca7ae5615a2681daa6e55092af8075efed
larsyunker <lars.yunker@gmail.com>
2016-09-15 15:25:42 -0700

    v4.1
    - updated function call to reflect current version of mzML
    - rewrote lines to reflect current versions of mzML and XLSX

--- spectrum binner.py
+++ spectrum binner.py
@@ -1,24 +1,10 @@
 """
 This script generates then iterates through a mzML file, binning a specified number of spectra
-v02
-new:
-    no longer requires all spectra to be pulled
-    bins spectra as each is pulled
-    fixed specified scan range trigger for output
-    ---02---
-    works with _mzML
-    specific functions (now in tome) are called instead of *
-    ---03---
-    removed save spectra function (now in XLSX)
-    cleaned up calls
-    ---4.0---
-    ---4.1
-
-to add:
-    output for more than one spectrum (MSMS ramping functionality)
+CHANGELOG:
+    ---4.2
 """
 
-def sumspectra(filename,sr='all',excel=None):
+def sumspectra(filename,start=None, end=None,excel=None):
     """
     Sums spectra from raw file and outputs to excel file 
     
@@ -36,18 +22,20 @@ def sumspectra(filename,sr='all',excel=None):
     
     st = ScriptTime()
     st.printstart()
-    mzml = mzML(filename[:-4]+'.mzML') # create mzML object
-    summed,sr = mzml.sumscans(sr)
-    xlfile = XLSX(filename[:-4],create=True)
-    xlfile.writespectrum(summed[0],summed[1],'summed spectra (scans %d-%d)' %(sr[0],sr[1]))
+    mzml = mzML(filename) # create mzML object
+    if start is None:
+        start = mzml.functions[1]['sr'][0]+1
+    if end is None:
+        end = mzml.functions[1]['sr'][1]+1
+    x,y = mzml.sum_scans(start=start,end=end)
+    xlfile = XLSX(filename,create=True)
+    xlfile.writespectrum(x,y,'summed spectra (scans %d-%d)' %(start,end))
     xlfile.save()
     st.printend()
     
 if __name__ == '__main__':
-    # raw filename to use
-    filename = 'LY-2016-06-13 09.raw'
-    
-    # scan range to sum (default 'all')
-    sr = 'all'
-    
-    sumspectra(filename,sr=sr)    
+    sumspectra(
+    'LY-2015-01-20 02', # raw filename to use
+    start = None, # start scan number
+    end = None, # end scan number
+    )    
