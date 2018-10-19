0450ddf6a97c8cfe92fc92810c705066d61a7c50
larsyunker <lars.yunker@gmail.com>
2018-10-19 09:53:34 -0700

    refactor to pip8 and current methods

--- ED-ESI plot.py
+++ ED-ESI plot.py
@@ -3,8 +3,8 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 from scipy import *
 
-from pythoms.classes import mzML
-from pythoms.tome import binnspectra,bincidspectra,version_input
+from pythoms.mzml import mzML
+from pythoms.tome import binnspectra, bincidspectra
 
 """
 for use with EDESI pulling
@@ -23,15 +23,19 @@ new:
     To Change -> Implement a generalized version of EDESIPlot for
                  use cases outside the _mzML framework
 """
+
+
 #######DEBUG FUNCTION#########
 
 def debugPrint(isDebug, printStr):
-    if(isDebug):
-       print printStr  
+    if (isDebug):
+        print(printStr)
+
 
 def anon(x):
-   #Used for testing lambda functions
-   print x
+    # Used for testing lambda functions
+    print(x)
+
 
 ######HELPER FUNCTION#########
 def GraphCleanUp(ax):
@@ -40,32 +44,39 @@ def GraphCleanUp(ax):
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
+
 def RoundToTen(val):
-    return int(ceil(val/10.0))*10
+    return int(ceil(val / 10.0)) * 10
+
 
 def RoundToTenFloor(val):
-    return int(floor(val/10.0))*10
+    return int(floor(val / 10.0)) * 10
+
 
 def RoundToHundred(val):
-    return int(ceil(val/100.0))*100
+    return int(ceil(val / 100.0)) * 100
+
 
 def round1sig(yTick):
     for i in range(0, len(yTick)):
-        #stackoverflow: 3410976
+        # stackoverflow: 3410976
         if (yTick[i] == 0):
             continue
         yTick[i] = round(yTick[i], -int(floor(log10(abs(yTick[i])))))
     return yTick
 
+
 def Valueround1sig(yTick):
     return round(yTick, -int(floor(log10(abs(yTick)))))
+
 
 def normalizeMSSpectrum(mzList, maxValue):
     relInt = []
     for i in mzList:
-       relInt.append((float(i)/float(maxValue))*100)
-       #print ((float(i)/float(maxValue))*100)
+        relInt.append((float(i) / float(maxValue)) * 100)
+        # print ((float(i)/float(maxValue))*100)
     return relInt
+
 
 #####################################
 
@@ -87,95 +98,94 @@ def normalizeMSSpectrum(mzList, maxValue):
 # No Return Object -> Saves Figure to Current Working Directory (CWD)
 #########################################################################
 def PlotEDESI(SummedSpectrum, ContourPlot, BreakdownCurve=False, **kwargs):
-    
-    #EDESI Plot Production variable
+    # EDESI Plot Production variable
     outputFile = './DYLYTest'
     EDESIkwargs = {
-        'minFilter': 20, # minFilter intensity value
-        'threshold': 1100, # threshold of peak height for Breakdown tracing
-        'plotZoom': True, # Construct Plot with Zoom in region of interest? (Autozoom)
-        'debug' : True
+        'minFilter': 20,  # minFilter intensity value
+        'threshold': 1100,  # threshold of peak height for Breakdown tracing
+        'plotZoom': True,  # Construct Plot with Zoom in region of interest? (Autozoom)
+        'debug': True
     }
     if set(kwargs.keys()) - set(EDESIkwargs.keys()):
         for i in set(kwargs.keys()) - set(EDESIkwargs.keys()):
-            raise KeyError('Unsupported keyword argument: {errKWarg}'.format(errKWarg = str(i)))
+            raise KeyError('Unsupported keyword argument: {errKWarg}'.format(errKWarg=str(i)))
     EDESIkwargs.update(kwargs)
-
 
     line_colours = ('BlueViolet', 'Crimson', 'ForestGreen', 'Indigo', 'Tomato', 'Maroon')
     levels = arange(minFilter, max(array(ContourPlot[2]).max(axis=1)), 6)
 
-    subplotVal = 1 #Contour Plot position
-    if(BreakdownCurve):
+    subplotVal = 1  # Contour Plot position
+    if (BreakdownCurve):
         subplotVal = 2
 
-    plt.figure(figsize=(8,9)) #Figure Size 8'', 9''
-    matplotlib.rcParams.update({'font.size':16})
+    plt.figure(figsize=(8, 9))  # Figure Size 8'', 9''
+    matplotlib.rcParams.update({'font.size': 16})
     matplotlib.rcParams['xtick.direction'] = 'out'
     matplotlib.rcParams['ytick.direction'] = 'out'
 
-    gs1 = gridspec.GridSpec(subplotVal,2,width_ratios=[3,1], height_ratios=[1,3]) #Optimal Dimension
+    gs1 = gridspec.GridSpec(subplotVal, 2, width_ratios=[3, 1], height_ratios=[1, 3])  # Optimal Dimension
     gs1.update(wspace=0.025, hspace=0.025)
 
     ## Contour Plot -> ax2 ##
     debugPrint(EDESIkwargs['debug'], "Contour Start")
-    
+
     ax2 = plt.subplot(gs1[subplotVal])
-    
-    ax2.contour(ContourPlot[0], ContourPlot[1], ContourPlot[2], levels, colors = 'k')
-    ContouryTick = arange(0, Valueround1sig(max(ContourPlot[1]))+10, 10)
-    #Labels the smallest value of the axis and then continue labelling with the arange increment ie 50, 750, 1500, 2250 rather than 50, 800, 1550, 2300 (tl;dr 50 does not affect the arange values)
-    ax2.set_yticks(concatenate(([Valueround1sig(min(ContourPlot[1]))],ContouryTick[:-1]), axis=0))
-    #ContourxTick = arange(0, Valueround1sig(max(binArr.keys()))+750, 750) #Unused
+
+    ax2.contour(ContourPlot[0], ContourPlot[1], ContourPlot[2], levels, colors='k')
+    ContouryTick = arange(0, Valueround1sig(max(ContourPlot[1])) + 10, 10)
+    # Labels the smallest value of the axis and then continue labelling with the arange increment ie 50, 750, 1500, 2250 rather than 50, 800, 1550, 2300 (tl;dr 50 does not affect the arange values)
+    ax2.set_yticks(concatenate(([Valueround1sig(min(ContourPlot[1]))], ContouryTick[:-1]), axis=0))
+    # ContourxTick = arange(0, Valueround1sig(max(binArr.keys()))+750, 750) #Unused
     ax2.set_ylabel('Collision Energy (V)')
     ax2.set_xlabel('m/z', style='italic')
     GraphCleanUp(ax2)
-    
+
     debugPrint(EDESIkwargs['debug'], "Contour End")
 
     ## Summed Spectrum -> ax1 ##
     debugPrint(EDESIkwargs['debug'], "Sum Spec Start")
-    
+
     ax1 = plt.subplot(gs1[0], sharex=ax2)
     sumSpecX, sumSpecY = SummedSpectrum
     ax1.plot(sumSpecX, sumSpecY)
     plt.title('Energy Dependent-ESI MS/MS Plot M={MINFIL}'.format(MINFIL=minFilter), y=1.08)
     plt.setp(ax1.get_xticklabels(), visible=False)
     ax1.set_ylabel('Intensity (%)')
-    #stackoverflow: 11244514
-    #ax1.set_yticks([0, 50, 100]) #Relative Intensity`
+    # stackoverflow: 11244514
+    # ax1.set_yticks([0, 50, 100]) #Relative Intensity`
     GraphCleanUp(ax1)
     ax1.spines['bottom'].set_visible(False)
     ax1.xaxis.set_ticks_position('none')
-    
+
     debugPrint(EDESIkwargs['debug'], "Sum Spec End")
 
     ## Zooming into particular m/z ##
     if (plotZoom):
         StartRange = min(map(float, sumSpecX))
         EndRange = max(map(float, sumSpecX))
-        Range = (EndRange-StartRange)/10
+        Range = (EndRange - StartRange) / 10
         xMinVal = StartRange - Range
         xMaxVal = EndRange + Range
-        xtickRange = arange(RoundToHundred(StartRange)-100, RoundToHundred(EndRange)+100, 100)
+        xtickRange = arange(RoundToHundred(StartRange) - 100, RoundToHundred(EndRange) + 100, 100)
         ax2.set_xticks(xtickRange)
         ax2.set_xlim(xmin=xMinVal, xmax=xMaxVal)
         ax1.set_xlim(xmin=xMinVal, xmax=xMaxVal)
 
     ## Constructing Breakdown Curve -> ax3 ##
     traceBank = {}
-    print BreakdownCurve
-    if(BreakdownCurve):
+    print
+    BreakdownCurve
+    if (BreakdownCurve):
         debugPrint(EDESIkwargs['debug'], "Breakdown Start")
-        
-        for mz in xrange(len(SummedSpectrum[0])):
+
+        for mz in range(len(SummedSpectrum[0])):
             if SummedSpectrum[1][mz] > threshold:
                 """
                 the combination of affinity:- and level:2 should enable the species to be summed (untested)
                 if that doesn't work, try including 'function':3, which should force the function to relate those species to that function number
                 let me know if this doesn't work
                 """
-                subDict = {'bounds': [int(SummedSpectrum[0][mz])], 'function':fn} 
+                subDict = {'bounds': [int(SummedSpectrum[0][mz])], 'function': fn}
                 traceBank[int(SummedSpectrum[0][mz])] = subDict
         outTrace = mzml.pull_species_data(traceBank)
         ax3 = plt.subplot(gs1[3], sharey=ax2)
@@ -186,52 +196,57 @@ def PlotEDESI(SummedSpectrum, ContourPlot, BreakdownCurve=False, **kwargs):
         ax3.set_xlabel('Intensity (%)')
         ax3.set_xticks([100])
         GraphCleanUp(ax3)
-        
+
         debugPrint(EDESIkwargs['debug'], "Breakdown End")
 
-    plt.savefig('../{OUTFILE}.png'.format(OUTFILE = outputFile+str(minFilter)), bbox_inches='tight')
+    plt.savefig('../{OUTFILE}.png'.format(OUTFILE=outputFile + str(minFilter)), bbox_inches='tight')
+
+
 ##################################
 
 ###############################################################
-#MAIN
+# MAIN
 ###############################################################
-#_mzML processing variables
-filename = 'HZ-140516_HOTKEYMSMS 1376 II.raw' # raw or mzml file name
-fillzeros = True # fills spectrum with zeros
-decpl = 1 # number of decimal places to track
-mzrange = None # mzrange to track
-sr = 'all' # scan range to track
-mzml = mzML(filename,verbose=True)
+# _mzML processing variables
+filename = 'HZ-140516_HOTKEYMSMS 1376 II.raw'  # raw or mzml file name
+fillzeros = True  # fills spectrum with zeros
+decpl = 1  # number of decimal places to track
+mzrange = None  # mzrange to track
+sr = 'all'  # scan range to track
+mzml = mzML(filename, verbose=True)
 
-#EDESI Plot Production variable
-minFilter = 20 # minFilter intensity value
-threshold = 1156 # threshold of peak height for Breakdown tracing
-plotBreakdown = True # Construct Plot with Breakdown?
-plotZoom = True # Construct Plot with Zoom in region of interest? (Autozoom)
+# EDESI Plot Production variable
+minFilter = 20  # minFilter intensity value
+threshold = 1156  # threshold of peak height for Breakdown tracing
+plotBreakdown = True  # Construct Plot with Breakdown?
+plotZoom = True  # Construct Plot with Zoom in region of interest? (Autozoom)
 
 msmsfns = []
-for func in mzml.functions: # identify MSMS functions in the provided file
+for func in mzml.functions:  # identify MSMS functions in the provided file
     if mzml.functions[func]['type'] == 'MS' and mzml.functions[func]['level'] > 1:
         msmsfns.append(func)
-if len(msmsfns) > 1: # if there is more than one msms function, ask the user which one to process
-    sys.stdout.write('More than one MS/MS function is contained in this mzML file. Please indicate which one you wish to process:\nFunction\ttarget\n')
+if len(msmsfns) > 1:  # if there is more than one msms function, ask the user which one to process
+    sys.stdout.write(
+        'More than one MS/MS function is contained in this mzML file. Please indicate which one you wish to process:\nFunction\ttarget\n')
     for func in msmsfns:
-        sys.stdout.write('%d\t%.3f\n' %(func,mzml.functions[func]['target']))
-    fn = version_input('function: ')
+        sys.stdout.write('%d\t%.3f\n' % (func, mzml.functions[func]['target']))
+    fn = input('function: ')
 else:
     fn = msmsfns[0]
 
 # pull time list, each spectrum at those time points, the scan range, and the mz range
-msms = mzml.retrieve_scans(fn=fn) # retrieve scans
-celist = mzml.functions[fn]['ce'] # collision energy list for the scans that were pulled
-limits = mzml.functions[fn]['window'] # m/z window
-zvals = [] # intensity matrix
+msms = mzml.retrieve_scans(fn=fn)  # retrieve scans
+celist = mzml.functions[fn]['ce']  # collision energy list for the scans that were pulled
+limits = mzml.functions[fn]['window']  # m/z window
+zvals = []  # intensity matrix
 
-binned = binnspectra(msms, len(msms), dec=decpl, startmz=limits[0], endmz=limits[1]) # bins all spectra together
-grouped,voltage = bincidspectra(msms, celist, decpl, limits[0], limits[1], minFilter, fillzeros) # bins spectra together by collision voltage
-for ind,scan in enumerate(grouped):
-    sys.stdout.write('\rExtracting z values #%d/%d (%.1f%%) ' %(ind+1,len(grouped),float(ind+1)/float(len(grouped))*100.))
-    zvals.append(scan[1]) # append only the intensities to list
+binned = binnspectra(msms, len(msms), dec=decpl, startmz=limits[0], endmz=limits[1])  # bins all spectra together
+grouped, voltage = bincidspectra(msms, celist, decpl, limits[0], limits[1], minFilter,
+                                 fillzeros)  # bins spectra together by collision voltage
+for ind, scan in enumerate(grouped):
+    sys.stdout.write(
+        '\rExtracting z values #%d/%d (%.1f%%) ' % (ind + 1, len(grouped), float(ind + 1) / float(len(grouped)) * 100.))
+    zvals.append(scan[1])  # append only the intensities to list
 sys.stdout.write(' DONE\n')
 
 """
@@ -256,13 +271,11 @@ binnspectra and bincidspectra have been rewritten to accept list of lists
 this means that the dictionary format of the outputs is no longer required, and can be simple list of lists (all ion keys are *hopefully* removed)
 
 """
-#ContourPlotLV = ""
-#for ion in msms:
-PlotEDESI(binned, 
-        [grouped[0][0], voltage, zvals], 
-        )
-    #print ion
-    #levels = arange(minFilter, max(array(zvals[ion]).max(axis=1)), 6)
-    #ContourPlotLV = plt.contour(grouped[ion][0][0], sorted(voltage[ion]), zvals[ion], levels, color='k')
-
-
+# ContourPlotLV = ""
+# for ion in msms:
+PlotEDESI(binned,
+          [grouped[0][0], voltage, zvals],
+          )
+# print ion
+# levels = arange(minFilter, max(array(zvals[ion]).max(axis=1)), 6)
+# ContourPlotLV = plt.contour(grouped[ion][0][0], sorted(voltage[ion]), zvals[ion], levels, color='k')
