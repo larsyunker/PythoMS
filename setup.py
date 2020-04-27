e1a93c300a44a4ef37dee869f06a2f20d4bb8b8b
larsyunker <lars.yunker@gmail.com>
2020-04-27 10:32:10 -0700

    remove redundant numpy

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'scipy>=1.1.0',
         'sympy>=1.1.1',
         'obonet==0.2.5',  # they changed attribute names without deprecationwarnings, so only this version is verified
-        'numpy',
         'isospecpy>=2.0.2',
     ],
     keywords=KEYWORDS,
